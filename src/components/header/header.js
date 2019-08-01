adecc0354257a2015cf4b3b35c2b2c48452c2e9f
sean donnelly <seandonnelly@ip-192-168-149-91.eu-west-1.compute.internal>
2019-08-01 17:58:19 +0100

    bit more bootstrappy

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,12 +1,14 @@
 import React from "react";
 import styles from "./header.module.scss";
+import { Link } from 'react-router-dom'
+
 
 function Header(props) {
   return (
     <div className={styles.bgWrapper}>
       <div className={styles.textWrapper}>
         <h1>{props.text}</h1>
-        <h3>[<a href="#soft">Lorum</a>] & [<a href="#photo">Ipsum</a>]</h3>
+        <h3> <Link to="/">Software</Link> & <Link to="/foto">Photography</Link></h3>
       </div>
     </div>
   );
--- src/components/home/home.js
+++ src/components/home/home.js
@@ -4,9 +4,9 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
-import styles from './home.module.scss';
+// import styles from './home.module.scss';
 
-function Home(props) {
+function Home() {
 
     return(
         <Container>
@@ -15,28 +15,28 @@ function Home(props) {
                     <h2>
                         Lorum Ipsum
                     </h2>
-                    <span>
+                    <p>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
                         Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
                         Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </span>  
+                    </p>  
                 </Col>
             </Row>
 
-            <PageBrake></PageBrake>
-            
+            {/* <PageBrake></PageBrake>
+             */}
             <Row>   
                 <Col>
                     <h2>
                         Lorum Ipsum
                     </h2>
-                    <span>
+                    <p>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
                         Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
                         Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </span>  
+                    </p>  
                 </Col>
             </Row>
         </Container>
